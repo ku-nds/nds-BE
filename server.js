668d753c854709a6bda4a8899946fdebf1d2673d
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-12-17 17:05:15 +0900

    fix: port 8000으로 변경

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use((err, req, res, next) => {
   res.status(err?.status || 500).json({ message: err?.message || 'Internal Server Error' });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 export default app;
